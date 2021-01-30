b7d7ce0582a727c125a6b532f7ffff9082d3fce0
Kosei Moriyama <cou929@gmail.com>
2021-01-31 00:03:09 +0900

    fix status description

--- ws/status.go
+++ ws/status.go
@@ -6,30 +6,37 @@ const (
 	StatusNormalClosure           = 1000
 	StatusGoingAway               = 1001
 	StatusProtocolError           = 1002
-	StatusNotAcceptable           = 1003
-	StatusNoStatusCode            = 1005
+	StatusUnsupportedData         = 1003
+	StatusNoStatusReceived        = 1005
 	StatusAbnormalClosure         = 1006
-	StatusInconsistentMessageType = 1007
+	StatusInvalidFramePayloadData = 1007
 	StatusPolicyViolation         = 1008
-	StatusTooLarge                = 1009
-	StatusNoExtension             = 1010 // client only
-	StatusUnexpectedCondition     = 1011
-	StatusTLSHandShakeFailure     = 1015
+	StatusMessageTooBig           = 1009
+	StatusMissingExtension        = 1010 // client only
+	StatusInternalError           = 1011
+	StatusServiceRestart          = 1012
+	StatusTryAgainLater           = 1013
+	StatusBadGateway              = 1014
+	StatusTLSHandShake            = 1015
 )
 
+// name according to https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
 var statusText = map[int]string{
 	StatusNormalClosure:           "Normal Closure",
 	StatusGoingAway:               "Going Away",
 	StatusProtocolError:           "Protocol Error",
-	StatusNotAcceptable:           "Not Acceptable",
-	StatusNoStatusCode:            "No Status Code",
+	StatusUnsupportedData:         "Unsupported Data",
+	StatusNoStatusReceived:        "No Status Received",
 	StatusAbnormalClosure:         "Abnormal Closure",
-	StatusInconsistentMessageType: "Inconsistent Message Type",
+	StatusInvalidFramePayloadData: "Invalid frame payload data",
 	StatusPolicyViolation:         "Policy Violation",
-	StatusTooLarge:                "Too Large",
-	StatusNoExtension:             "No Extension",
-	StatusUnexpectedCondition:     "Unexpected Condition",
-	StatusTLSHandShakeFailure:     "TLS HandShake Failure",
+	StatusMessageTooBig:           "Message too big",
+	StatusMissingExtension:        "Missing Extension",
+	StatusInternalError:           "Internal Error",
+	StatusServiceRestart:          "Service Restart",
+	StatusTryAgainLater:           "Try Again Later",
+	StatusBadGateway:              "Bad Gateway",
+	StatusTLSHandShake:            "TLS HandShake",
 }
 
 // StatusText returns a text for the WebSocket status code
